4bf85e4f51783e98c930923ad348b589ee24dbfe
Ranjithrahith <30952736+Ranjithrahith@users.noreply.github.com>
2017-08-29 16:11:37 +0530

    Create evenorodd.c

---
+++ evenorodd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+    int;
+    printf("Enter an integer");
+    scanf("%d", &number);
+    if(number % 4 == 0)
+       {
+       printf("%d",even number);
+       }
+    else
+    {
+        printf("%d",odd number);
+        }
+}
